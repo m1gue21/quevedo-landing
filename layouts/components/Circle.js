6b81663c985c4c220c910eaf5e62e96bd1ef205c
glocal-frontend-leader <73451596+m1gue21@users.noreply.github.com>
2024-11-21 21:20:41 -0500

    1st commit

---
+++ layouts/components/Circle.js
@@ -0,0 +1,13 @@
+function Circle({ className, width, height, fill = true, ...props }) {
+  return (
+    <div
+      className={`absolute ${className} ${
+        fill ? "bg-primary" : "bg-[#b6dffc]"
+      } rounded-full`}
+      style={{ width: `${width}px`, height: `${height}px` }}
+      {...props}
+    ></div>
+  );
+}
+
+export default Circle;
